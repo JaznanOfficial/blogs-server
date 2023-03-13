6114fc4eefd2dd6bd62c2eee5212311f2f64714e
JaznanOfficial <jaznanofficial@gmail.com>
2023-03-13 08:31:24 +0600

    first commit

---
+++ utils/dbConnect.js
@@ -0,0 +1,20 @@
+const mongoose = require("mongoose")
+const dotenv = require("dotenv").config()
+const colors = require("colors")
+
+const dbConnect = () => {
+    mongoose.connect(process.env.mongodb_connection, {
+        dbName:"blog-server"
+    })
+    
+        .then(() => {
+        console.log(`db connected succesfully`.white.bgGreen.bold)
+        }).catch((err) => {
+        console.log(err.message);
+    })
+}
+
+// blog-server
+// 5iQY7nJeeevrZIrt
+
+module.exports = dbConnect;
