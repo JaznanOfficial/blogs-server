7c9a7d9f8acdc7af79bebb61510a42333e8d4a2e
JaznanOfficial <jaznanofficial@gmail.com>
2023-05-10 12:01:05 +0600

    payment server done

--- app.js
+++ app.js
@@ -3,6 +3,8 @@ const app = express();
 const PORT = process.env.PORT || 5000;
 const cors = require("cors");
 const colors = require("colors");
+require("dotenv").config();
+const stripe = require("stripe")(process.env.stripe_secret_key);
 
 app.use(cors());
 app.use(express.json());
@@ -14,6 +16,43 @@ const blogsRoute = require("./v1/Routes/blogs.route");
 app.use("/api/v1/users", usersRoute);
 app.use("/api/v1/categories", categoriesRoute);
 app.use("/api/v1/blogs", blogsRoute);
+
+// stripe server of dream-shop code------------->
+
+app.post("/create-payment-page", async (req, res) => {
+    try {
+        const { amount, currency, name, quantity } = req.body;
+        console.log(quantity);
+
+        const session = await stripe.checkout.sessions.create({
+            payment_method_types: ["card"],
+            line_items: [
+                {
+                    price_data: {
+                        currency,
+                        unit_amount: amount,
+                        product_data: {
+                            name: "Total Amount",
+                        },
+                    },
+                    quantity: 1,
+                },
+            ],
+            mode: "payment",
+            success_url: "http://localhost:3000/payment-success",
+            cancel_url: "http://localhost:3000/",
+        });
+
+        res.send({
+            sessionId: session.id,
+        });
+    } catch (error) {
+        console.error(error);
+        res.status(500).json({ error: "An error occurred while creating the payment page" });
+    }
+});
+
+// stripe server of dream-shop code------------->
 
 app.get("/", (req, res) => {
     try {
